0a6ace6f67fd6db50af342b42bb92d12c09d7edf
Bartek5756 <101180607+Bartek5756@users.noreply.github.com>
2022-04-05 08:58:01 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    int w=5, i;
+    for (i=1; i<=w;i++){
+       for(int x=0; x<i ; x++)
+        System.out.print("*");
+          System.out.println(" ");
+    }
+  }
+}
